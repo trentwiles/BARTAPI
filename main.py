ba7e3b9b167e2b23bf95a59ada45aeb47fde51bc
trentwiles <wilest7@gmail.com>
2023-10-21 08:27:16 -0700

    docs

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, Response, make_response
+from flask import Flask, Response, make_response, render_template
 import bart
 import stations
 import alerts
@@ -14,31 +14,8 @@ def jsonResp(input, status):
 
 @app.route('/')
 def home():
-    return jsonResp({"error": False, "message": "Check out bart.trentwil.es for documentation"}, 200)
-
-"""
-GET /api/v1/getPredictions/:station Get Train Predictions/Arrival Times
-@apiName getPredictions
-
-@apiParam {String} station BART Station Abreviation
-
-@apiSuccess {String} firstname Firstname of the User.
-
-@apiSuccessExample Success-Response:
-    HTTP/1.1 200 OK
-    {
-        "firstname": "John",
-        "lastname": "Doe"
-    }
-
-@apiError UserNotFound The id of the User was not found.
-
-@apiErrorExample Error-Response:
-    HTTP/1.1 404 Not Found
-    {
-        "error": "UserNotFound"
-    }
-"""
+    return render_template("index.html")
+    #return jsonResp({"error": False, "message": "Check out bart.trentwil.es for documentation"}, 200)
 
 @app.route("/api/v1/getPredictions/<station>")
 def getPred(station):

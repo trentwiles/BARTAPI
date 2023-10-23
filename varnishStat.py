be4a7111488f7171aec73e176c66f8f0df03c0f3
trentwiles <wilest7@gmail.com>
2023-10-23 08:51:38 -0700

    varnish stats

--- varnishStat.py
+++ varnishStat.py
@@ -3,8 +3,8 @@ import json
 
 def cacheMissHit():
     api = json.loads(subprocess.check_output("varnishstat --json", shell=True, text=True))
-    hit = api["MAIN.cache_hit"]["value"]
-    miss = api["MAIN.cache_miss"]["value"]
+    hit = api["counters"]["MAIN.cache_hit"]["value"]
+    miss = api["counters"]["MAIN.cache_miss"]["value"]
     ratio = hit / (miss + hit)
     return {"cacheHits": hit, "cacheMiss": miss, "hitRatio": ratio}
 

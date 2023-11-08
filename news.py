ecc5e4cc5690dd24d7bb156d6e04bb35b9246940
trentwiles <wilest7@gmail.com>
2023-11-08 09:32:19 -0800

    news scraper code

--- news.py
+++ news.py
@@ -1,8 +1,9 @@
 import requests
 from bs4 import BeautifulSoup
+from markdownify import markdownify as md
 
-def getNewsTitles():
-    r = requests.get("https://www.bart.gov/news/articles")
+def getTitles(endpoint):
+    r = requests.get(endpoint)
     soup = BeautifulSoup(r.text, "html.parser")
 
     d = []
@@ -18,4 +19,29 @@ def getNewsTitles():
             fds = ""
     return d
 
-print(getNewsTitles())
+def getLatestTitles():
+    return getTitles("https://www.bart.gov/news/articles")
+
+def getTitlesByYear(year):
+    return getTitles(f"https://www.bart.gov/news/articles/{str(year)}")
+
+def getArticleContent(link):
+    # 
+    r = requests.get(link)
+    soup = BeautifulSoup(r.text, "html.parser")
+
+
+    # add https://www.bart.gov in front of each URL
+    # we are assuming the rendered markdown/html will not be displayed on BART's website
+    for link in soup.find_all('a'):
+        href = link.get('href')
+        if href and not href.startswith('http'):
+            link['href'] = 'https://www.bart.gov' + href
+
+    rawL = soup.find("div", {"class": "field field--field-legacy-body-content field--string-long"}).contents
+    raw = ""
+    for x in rawL:
+        if x != "\n":
+            raw += str(x)
+
+    return {"title": soup.title.text, "date": soup.find("div", {"class": "field field--field-news-date field--datetime"}).text.strip(), "html": raw, "markdown": md(raw)}
